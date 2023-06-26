f49c14ccfe6c6dc40d73ac02f3227ff7681d3aa3
ShaneKoNaung <shanekonaung@gmail.com>
2023-06-27 01:06:00 +0630

    Q5 done

--- Homeworks/Homework4/starter.py
+++ Homeworks/Homework4/starter.py
@@ -1,32 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().system('pip freeze | grep scikit-learn')
-
-
-# In[2]:
-
-
 import pickle
 import pandas as pd
+from pathlib import Path
+import argparse
 
 
-# In[3]:
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('year', type=int)
+parser.add_argument('month', type=int)
+
+args = parser.parse_args()
 
 
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
 
-# In[4]:
-
-
 categorical = ['PULocationID', 'DOLocationID']
 
+
 def read_data(filename):
+    
     df = pd.read_parquet(filename)
     
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
@@ -39,63 +34,23 @@ def read_data(filename):
     return df
 
 
-# In[6]:
+if __name__ == "__main__":
+
+    year = args.year
+    month = args.month
+
+    data_url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+
+    df = read_data(data_url)
 
 
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-02.parquet')
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
 
 
-# In[7]:
+    print(f"The mean of the predicted duration of this dataset is {y_pred.mean()}")
 
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
-
-
-# # Q1. Notebook
-# 
-
-# In[10]:
-
-
-print(f"The standard deviation of the predicted duration of this dataset is {y_pred.std()}")
-
-
-# # Q2. Preparing the output
-# 
-
-# In[18]:
-
-
-year = 2022
-month = 2
-output_file = f"output_yellow_tripdata_{year:04d}-{month:02d}.parquet"
-
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-df_result = pd.DataFrame({'ride_id' : df['ride_id'], 'preds' :  y_pred})
-
-df_result.to_parquet(
-    output_file,
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
-
-# In[25]:
-
-
-from pathlib import Path
-
-print(f"The size of the output file is {Path(output_file).stat().st_size / (1024 * 1024):2f}")
-
-
-# # Q3. Creating the scoring script
-# 
-
-# In[ ]:
 
 
 
